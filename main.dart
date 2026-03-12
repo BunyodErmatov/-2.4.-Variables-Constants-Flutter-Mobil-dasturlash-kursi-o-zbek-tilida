7706459312572561d9faf0e183cc857b7846c42b
Bunyodjon Ermatov <bunyod@example.com>
2026-03-12 13:24:35 +0500

    Dars topshirig'i

---
+++ main.dart
@@ -0,0 +1,18 @@
+void main(List<String> args) {
+  const myAge = 27;
+  print(myAge);
+
+  double averageAge = 27;
+  int myFather = 62;
+  int myMother = 59;
+  int myBrother = 29;
+
+  averageAge = (averageAge + myFather + myMother + myBrother) / 4;
+
+  print(averageAge);
+
+  const testNumber = 15;
+  int result = testNumber % 2;
+
+  print(result);
+}
